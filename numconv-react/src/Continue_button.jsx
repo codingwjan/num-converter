eea1205b396b13dd17a44725721d7f393a64560f
ebayboy123 <107748760+ebayboy123@users.noreply.github.com>
2022-10-14 16:59:02 +0200

    nothing changed

--- numconv-react/src/Continue_button.jsx
+++ numconv-react/src/Continue_button.jsx
@@ -5,24 +5,27 @@ import { blue } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: blue[400],
-    },
+palette: {
+  primary: {
+    main: blue[400],
   },
+},
 });
 
-
+function greet() {
+  console.log('click');
+}
 
 function ContinueButton() {
     return (
     <React.StrictMode>
         <ThemeProvider theme={theme}>
         <div className='cont_button'>
-            <Button variant='contained' size="large" style={{
+            <Button variant='contained' size="large"
+            style={{
             fontSize: '25px',
           }}
-          onClick={event =>  window.location.href='MainPage.js'}
+          onClick="greet()"
           >Continue</Button>;
         </div>
     </ThemeProvider>
--- numconv-react/src/mainpage.jsx
+++ numconv-react/src/mainpage.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { ReactDOM } from "react";
+import * as React from 'react';
 
 const MainPage = () => {
     return (
         <>
-        <h1>hello world</h1>
+            <h1>hello world</h1>
         </>
     )
 }
